6305dbb392e37def845ff78733d2fa1afb946e3c
VillalobosLobo <130194331+VillalobosLobo@users.noreply.github.com>
2023-10-11 19:48:28 -0600

    Add files via upload

---
+++ funciones.py
@@ -0,0 +1,29 @@
+import os
+os.system('clear')
+
+def aee(a, b):
+    if b == 0:
+        return (a, 1, 0)
+    else:
+        (d, x1, y1) = aee(b, a % b)
+        x = y1
+        y = x1 - (a // b) * y1
+        return (d, x, y)
+
+#Variables de entrada
+print('Ingresa n : ',end='')
+n=int(input())
+
+print('Ingresa alfa : ',end='')
+alfa=int(input())
+
+print('Ingresa beta : ',end='')
+beta=int(input())
+
+mcd,x,y=aee(alfa,n)
+
+print('\nFunción de cifrado\nc='+str(alfa)+'p + '+str(beta)+' mod('+str(n)+')')
+
+print('\nFunción de decifrado\np='+str(x)+' [c+'+str(n-beta)+'] mod('+str(244)+')')
+
+
