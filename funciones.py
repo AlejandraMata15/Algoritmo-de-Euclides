8ccb37c8669acc7a4200d22cb6a5f33d6d1f73bb
AlejandraMata15 <93355429+AlejandraMata15@users.noreply.github.com>
2023-10-11 22:30:56 -0600

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -12,7 +12,8 @@ def aee(a, b):
 
 def inicio(n,alfa,beta):
 	mcd,x,y=aee(alfa,n)
-	cifrado_label.config(text="\nFunción de cifrado'\nc=" + str(alfa)+"p + "+str(beta)+" mod("+str(n))
-	descifrado_label.config(text="\nFunción de decifrado\np="+str(x)+" [c+" +str(n-beta)+"] mod("+str(n)+")")
+	cifrado='\nFunción de cifrado\nc='+str(alfa)+'p + '+str(beta)+' mod('+str(n)+')'
+	descifrado='\nFunción de decifrado\np='+str(x)+' [c+'+str(n-beta)+'] mod('+str(n)+')'
 	print('\nFunción de cifrado\nc='+str(alfa)+'p + '+str(beta)+' mod('+str(n)+')')
 	print('\nFunción de decifrado\np='+str(x)+' [c+'+str(n-beta)+'] mod('+str(n)+')')
+	return (cifrado,descifrado)
