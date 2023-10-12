3800a1ecaa74e7d1cd231fedaaedf26fb397d096
VillalobosLobo <130194331+VillalobosLobo@users.noreply.github.com>
2023-10-11 19:29:35 -0600

    Update funciones.py
    Funciones de decifrado y cifrado funcionando

--- funciones.py
+++ funciones.py
@@ -1,11 +1,11 @@
 import os
 os.system('clear')
 
-def euclides_extendido(a, b):
+def aee(a, b):
     if b == 0:
         return (a, 1, 0)
     else:
-        (d, x1, y1) = euclides_extendido(b, a % b)
+        (d, x1, y1) = aee(b, a % b)
         x = y1
         y = x1 - (a // b) * y1
         return (d, x, y)
@@ -20,7 +20,11 @@ alfa=int(input())
 print('Ingresa beta : ',end='')
 beta=int(input())
 
-mcd,x,y=euclides_extendido(alfa,n)
+mcd,x,y=aee(alfa,n)
+
+print('\nFunción de cifrado\nc='+str(alfa)+'p + '+str(beta)+' mod('+str(n)+')')
+
+print('\nFunción de decifrado\np ='+str(x)+' [c+'+str(n-beta)+'] mod('+str(n)+') ')
 
 print('\nFunción de cifrado\n'+str(mcd)+' = '+str(alfa)+' + '+str(beta)+' mod('+str(n)+')')
 
